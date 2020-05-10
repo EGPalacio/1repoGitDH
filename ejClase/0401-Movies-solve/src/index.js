32822f8e0eafc82c019c0095d9fc3a27fa17c9c7
EGPalacio <gastonpalacio@hotmail.com>
2020-05-10 02:07:24 -0300

    ej NodeJS: Movies

---
+++ ejClase/0401-Movies-solve/src/index.js
@@ -0,0 +1,15 @@
+const home = require('./homePage');
+const enCartelera = require('./enCartelera');
+const masVotadas = require('./masVotadas');
+const sucursales = require('./sucursales');
+const contacto = require('./contacto');
+const faqs = require('./preguntasFrecuentes');
+
+module.exports = {
+    home,
+    enCartelera,
+    masVotadas,
+    sucursales,
+    contacto,
+    faqs,
+}
